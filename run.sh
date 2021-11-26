d59f505d59cdf8d24253f61586192e152e01f4d3
b1 <alch3mist94@protonmail.com>
2021-11-26 03:02:19 +0100

    small changes

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Raptoreum setup and deploy script with CLI.
 ###################### HEAD ########################
-name="TheBoyz"
+name="cpu-limit"
 version="1.2.4.1" 
 ####################################################
 
